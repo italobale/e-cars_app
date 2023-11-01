bc65bb3f3ad4ff07f1821e450fbfa654db7a52fd
Italo Balestra <italo.balestra@olt-dss.com>
2023-11-01 17:51:53 +0100

    Modified credit

--- app.py
+++ app.py
@@ -176,5 +176,5 @@ with container2:
     st.table(ss)
 
     st.markdown(
-        "[Data source](https://docs.google.com/spreadsheets/d/1V6ucyFGKWuSQzvI8lMzvvWJHrBS82echMVJH37kwgjE/edit?usp=sharing) - All credit goes to [Bjørn Nyland](https://www.youtube.com/user/bjornnyland) for performing extensive testing and making the data publicly available."
+        "Credit goes to [Bjørn Nyland](https://www.youtube.com/user/bjornnyland) for performing extensive testing and making the [data publicly available](https://docs.google.com/spreadsheets/d/1V6ucyFGKWuSQzvI8lMzvvWJHrBS82echMVJH37kwgjE/edit?usp=sharing)."
     )
